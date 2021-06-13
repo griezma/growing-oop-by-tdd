90322517f166e4e64ef3160e13e09d4d64ef0aa8
griezma <griezma@outlook.com>
2021-06-13 12:02:58 +0200

    Openfire as docker-compose

--- start_openfire_docker.bat
+++
@@ -1,3 +0,0 @@
-mkdir %CD%\docker-volumes\openfire
-
-docker run --name openfire -d --publish 9090:9090 --publish 5222:5222 --publish 7777:7777 --volume %CD%/docker-volumes/openfire:/var/lib/openfire sameersbn/openfire:3.10.3-19
--- start_openfire_docker.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p $PWD/docker-volumes/openfire
-
-docker run --name openfire -d --restart=always \
-  --publish 9090:9090 --publish 5222:5222 --publish 7777:7777 \
-  --volume $PWD/docker-volumes/openfire:/var/lib/openfire \
-  sameersbn/openfire:3.10.3-19
